9cc98afc6e8ef8f1664974182f7f314752772380
bynned <benny.cascarino@tuni.fi>
2023-05-19 12:00:21 +0300

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,32 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="title-header">Forum</h1>
-      <div className="center-content">
-        <p>
-          Welcome! <br /> Please log in or register to enter!
-        </p>
-        <div className="button-container">
-        <button>Log in</button>
-        <button>Register</button>
-        </div>
-      </div>
-      <br />
-      <br />
+      <Router>
+        <h1 className="title-header">Forum</h1>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/"
+            element={
+              <div className="center-content">
+                <p>
+                  Welcome! <br /> Please login or register to enter!
+                </p>
+                <div className="button-container">
+                  <Link to="/login" className="LinkButton">Login</Link>
+                  <Link to="/register" className="LinkButton">Register</Link>
+                </div>
+              </div>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
