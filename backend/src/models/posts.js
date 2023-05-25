af635723eb9d167e82fd3d5e7e9302e18d11c38a
bynned <benny.cascarino@tuni.fi>
2023-05-25 11:09:02 +0300

    Added the boilerplate of like and dislike

--- backend/src/models/posts.js
+++ backend/src/models/posts.js
@@ -1,39 +1,47 @@
-const mongoose = require('mongoose');
-const date = require('date-and-time');
+const mongoose = require("mongoose");
+const date = require("date-and-time");
 
 const now = new Date();
 const UTC = date.addHours(now, 3);
-const dateNtime = date.format(UTC, 'DD/MM/YYYY HH:mm:ss');
+const dateNtime = date.format(UTC, "DD/MM/YYYY HH:mm:ss");
 
 const postsSchema = new mongoose.Schema({
-    username: {
+  username: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: String,
+    default: dateNtime,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  dislikes: {
+    type: Number,
+    default: 0,
+  },
+  comments: [
+    {
+      username: {
         type: String,
-        required: true
-    },
-    content: {
+        required: true,
+      },
+      content: {
         type: String,
-        required: true
-    },
-    timestamp: {
+        required: true,
+      },
+      timestamp: {
         type: String,
-        default: dateNtime
+        default: dateNtime,
+      },
     },
-    comments: [
-        {
-          username: {
-            type: String,
-            required: true,
-          },
-          content: {
-            type: String,
-            required: true,
-          },
-          timestamp: {
-            type: String,
-            default: dateNtime
-          },
-        },
-      ],
+  ],
 });
 
-module.exports = mongoose.model('posts', postsSchema)
+module.exports = mongoose.model("posts", postsSchema);
