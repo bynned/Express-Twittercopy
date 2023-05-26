0b38611759f2299d04351123df2a77cecf2aef1d
bynned <benny.cascarino@tuni.fi>
2023-05-26 13:29:03 +0300

    Copy paste to the max, now the comment like and dislikes have the same system as the post ones.

--- backend/src/public/scripts/likeAndDislike.js
+++ backend/src/public/scripts/likeAndDislike.js
@@ -104,5 +104,104 @@ dislikeButton.addEventListener("click", () => {
 
 /*************************************************************************************************************************
  *************************************************************************************************************************
- *************************************************************************************************************************
+ ***************************DYNAMICALLY CHANGING THE AMOUNT OF LIKES ON COMMENTS USING AJAX*******************************
  ************************************************************************************************************************/
+
+
+const comlikeButton = document.getElementById("comlike-button");
+const comdislikeCount = document.querySelector(".comdislike-number");
+const comlikeCount = document.querySelector(".comlike-number");
+const comdislikeButton = document.getElementById("comdislike-button");
+
+comlikeButton.addEventListener("click", () => {
+  const commentId = comlikeButton.getAttribute("data-commentid");
+  const postId = likeButton.getAttribute("data-postid");
+
+  // Send a GET request to check if the user has liked/disliked the comment before
+  const xhrGet = new XMLHttpRequest();
+  xhrGet.open("GET", `/post/${postId}/${commentId}/like`, true);
+  xhrGet.onreadystatechange = function () {
+    if (xhrGet.readyState === XMLHttpRequest.DONE) {
+      if (xhrGet.status === 200) {
+        const response = JSON.parse(xhrGet.responseText);
+        // comhasLiked and comhasDisliked are boolean values from the server
+        const comhasLiked = response.comhasLiked;
+        const comhasDisliked = response.comhasDisliked;
+
+        if (comhasLiked) {
+          // if you already liked the comment
+          console.log("You already liked this comment");
+          return;
+        } else if (comhasDisliked) {
+          // If you had earlier disliked the comment and decided to like it,
+          // it will take your dislike away.
+          const comdislikeCount = document.querySelector(".comdislike-number");
+          comdislikeCount.textContent = parseInt(comdislikeCount.textContent) - 1;
+        }
+        const xhrPost = new XMLHttpRequest();
+        // Post to the server the like
+        xhrPost.open("POST", `/post/${postId}/${commentId}/like`, true);
+        xhrPost.setRequestHeader("Content-Type", "application/json");
+        xhrPost.onreadystatechange = function () {
+          if (xhrPost.readyState === XMLHttpRequest.DONE) {
+            if (xhrPost.status === 201) {
+              // Add +1 to the likes
+              const comlikeCount = document.querySelector(".comlike-number");
+              comlikeCount.textContent = parseInt(comlikeCount.textContent) + 1;
+            } else {
+              console.error("Error:", xhrPost.status);
+            }
+          }
+        };
+        xhrPost.send();
+      } else {
+        console.error("Error:", xhrGet.status);
+      }
+    }
+  };
+  xhrGet.send();
+});
+
+// This literally is a copy paste from above.
+comdislikeButton.addEventListener("click", () => {
+    const commentId = comdislikeButton.getAttribute("data-commentid");
+    const postId = dislikeButton.getAttribute("data-postid");
+
+    // Send a GET request to check if the user has liked/disliked the comment before
+    const xhrGet = new XMLHttpRequest();
+    xhrGet.open("GET", `/post/${postId}/${commentId}/like`, true);
+    xhrGet.onreadystatechange = function () {
+      if (xhrGet.readyState === XMLHttpRequest.DONE) {
+        if (xhrGet.status === 200) {
+          const response = JSON.parse(xhrGet.responseText);
+          const comhasLiked = response.comhasLiked;
+          const comhasDisliked = response.comhasDisliked;
+
+          if (comhasDisliked) {
+            console.log("You already disliked this post");
+            return;
+          } else if (comhasLiked) {
+            const comlikeCount = document.querySelector(".comlike-number");
+            comlikeCount.textContent = parseInt(comlikeCount.textContent) - 1;
+          }
+          const xhrPost = new XMLHttpRequest();
+          xhrPost.open("POST", `/post/${postId}/${commentId}/dislike`, true);
+          xhrPost.setRequestHeader("Content-Type", "application/json");
+          xhrPost.onreadystatechange = function () {
+            if (xhrPost.readyState === XMLHttpRequest.DONE) {
+              if (xhrPost.status === 201) {
+                const comdislikeCount = document.querySelector(".comdislike-number");
+                comdislikeCount.textContent = parseInt(comdislikeCount.textContent) + 1;
+              } else {
+                console.error("Error:", xhrPost.status);
+              }
+            }
+          };
+          xhrPost.send();
+        } else {
+          console.error("Error:", xhrGet.status);
+        }
+      }
+    };
+    xhrGet.send();
+  });
