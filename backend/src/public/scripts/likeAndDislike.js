df7a4548eeaaf2310b5fcb813ab24a3d677e3ec8
bynned <benny.cascarino@tuni.fi>
2023-05-26 23:49:05 +0300

    Added sort button (no functionality)

--- backend/src/public/scripts/likeAndDislike.js
+++ backend/src/public/scripts/likeAndDislike.js
@@ -210,3 +210,27 @@ comdislikeButtons.forEach((comdislikeButton) => {
     xhrGet.send();
   });
 });
+
+/**********************************************************************************
+***********************************************************************************
+***************************Dropdown menu for sorting comments**********************
+***********************************************************************************
+**********************************************************************************/
+
+function sortComments() {
+  document.getElementById("sort-comments").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
