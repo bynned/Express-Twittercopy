69e203be81641f2a583b98621673fc908cb20cc4
bynned <benny.cascarino@tuni.fi>
2023-05-26 12:23:17 +0300

    Now when you like or dislike a post, it will first GET from the server if the user has liked or disliked before in a boolean value

--- backend/src/public/scripts/likeAndDislike.js
+++ backend/src/public/scripts/likeAndDislike.js
@@ -14,52 +14,93 @@ let disliked = null;
 likeButton.addEventListener("click", () => {
   const postId = likeButton.getAttribute("data-postid");
 
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", `/post/${postId}/like`, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 201) {
-        const likeCount = document.querySelector(".like-number");
-        likeCount.textContent = parseInt(likeCount.textContent) + 1;
-        liked = true; // Now it's true that we have liked
-        // This is for dynamically taking away the dislike if you decide to like
-        if (disliked) {
+  // Send a GET request to check if the user has liked/disliked the post before
+  const xhrGet = new XMLHttpRequest();
+  xhrGet.open("GET", `/post/${postId}/like`, true);
+  xhrGet.onreadystatechange = function () {
+    if (xhrGet.readyState === XMLHttpRequest.DONE) {
+      if (xhrGet.status === 200) {
+        const response = JSON.parse(xhrGet.responseText);
+        // hasLiked and hasDisliked are boolean values from the server
+        const hasLiked = response.hasLiked;
+        const hasDisliked = response.hasDisliked;
+
+        if (hasLiked) {
+          // if you already liked the post
+          console.log("You already liked this post");
+          return;
+        } else if (hasDisliked) {
+          // If you had earlier disliked the post and decided to like it,
+          // it will take your dislike away.
           const dislikeCount = document.querySelector(".dislike-number");
           dislikeCount.textContent = parseInt(dislikeCount.textContent) - 1;
         }
+        const xhrPost = new XMLHttpRequest();
+        // Post to the server the like
+        xhrPost.open("POST", `/post/${postId}/like`, true);
+        xhrPost.setRequestHeader("Content-Type", "application/json");
+        xhrPost.onreadystatechange = function () {
+          if (xhrPost.readyState === XMLHttpRequest.DONE) {
+            if (xhrPost.status === 201) {
+              // Add +1 to the likes
+              const likeCount = document.querySelector(".like-number");
+              likeCount.textContent = parseInt(likeCount.textContent) + 1;
+            } else {
+              console.error("Error:", xhrPost.status);
+            }
+          }
+        };
+        xhrPost.send();
       } else {
-        console.error("Error:", xhr.status);
+        console.error("Error:", xhrGet.status);
       }
     }
   };
-  xhr.send();
+  xhrGet.send();
 });
 
+// This literally is a copy paste from the likebutton from above.
 dislikeButton.addEventListener("click", () => {
-  const postId = dislikeButton.getAttribute("data-postid");
+    const postId = dislikeButton.getAttribute("data-postid");
 
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", `/post/${postId}/dislike`, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 201) {
-        const dislikeCount = document.querySelector(".dislike-number");
-        dislikeCount.textContent = parseInt(dislikeCount.textContent) + 1;
-        disliked = true; // Now it's true that we have disliked
-        // This is for dynamically taking away the like if you decide to dislike
-        if (liked) {
-          const likeCount = document.querySelector(".like-number");
-          likeCount.textContent = parseInt(likeCount.textContent) - 1;
+    // Send a GET request to check if the user has liked/disliked the post before
+    const xhrGet = new XMLHttpRequest();
+    xhrGet.open("GET", `/post/${postId}/like`, true);
+    xhrGet.onreadystatechange = function () {
+      if (xhrGet.readyState === XMLHttpRequest.DONE) {
+        if (xhrGet.status === 200) {
+          const response = JSON.parse(xhrGet.responseText);
+          const hasLiked = response.hasLiked;
+          const hasDisliked = response.hasDisliked;
+
+          if (hasDisliked) {
+            console.log("You already disliked this post");
+            return;
+          } else if (hasLiked) {
+            const likeCount = document.querySelector(".like-number");
+            likeCount.textContent = parseInt(likeCount.textContent) - 1;
+          }
+          const xhrPost = new XMLHttpRequest();
+          xhrPost.open("POST", `/post/${postId}/dislike`, true);
+          xhrPost.setRequestHeader("Content-Type", "application/json");
+          xhrPost.onreadystatechange = function () {
+            if (xhrPost.readyState === XMLHttpRequest.DONE) {
+              if (xhrPost.status === 201) {
+                const dislikeCount = document.querySelector(".dislike-number");
+                dislikeCount.textContent = parseInt(dislikeCount.textContent) + 1;
+              } else {
+                console.error("Error:", xhrPost.status);
+              }
+            }
+          };
+          xhrPost.send();
+        } else {
+          console.error("Error:", xhrGet.status);
         }
-      } else {
-        console.error("Error:", xhr.status);
       }
-    }
-  };
-  xhr.send();
-})
+    };
+    xhrGet.send();
+  });
 
 /*************************************************************************************************************************
  *************************************************************************************************************************
