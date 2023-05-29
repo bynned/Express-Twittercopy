e5555c783ee043f306c5c58a2de680262fddaa2b
bynned <benny.cascarino@tuni.fi>
2023-05-29 14:53:42 +0300

    Basic boilerplate functionality

--- backend/src/public/scripts/channelView.js
+++ backend/src/public/scripts/channelView.js
@@ -2,3 +2,17 @@ window.addEventListener("load", function () {
     var div = document.getElementById("Appid");
     div.classList.add("appear");
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var createChannelButton = document.getElementById('createChannelButton');
+    var createChannelFormContainer = document.getElementById('createChannelFormContainer');
+    var createChannelForm = document.getElementById('createChannelForm');
+
+    createChannelButton.addEventListener('click', function() {
+        createChannelFormContainer.style.display = 'block';
+    });
+
+    createChannelForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+    })
+})
