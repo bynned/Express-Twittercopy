e5b02c00193e898a2b6b719ea8229b187c7bfc45
bynned <benny.cascarino@tuni.fi>
2023-05-29 15:09:33 +0300

    router.post

--- backend/src/routes/channelRouter.js
+++ backend/src/routes/channelRouter.js
@@ -2,4 +2,37 @@ const express = require("express");
 const router = express.Router();
 const channel = require("../models/channel");
 
+
+// Route for creating a new channel
+router.post("/channels", isAuthenticated, (req, res) => {
+    const channelName = req.body.channelName;
+
+    // Generate the href for the new channel
+    const href = "/" + channelName.toLowerCase().replace(/ /g, "-");
+
+    const newChannel = new channel({
+        name: channelName,
+        href: href
+    });
+
+    newChannel
+        .save()
+        .then(() => {
+            console.log("New channel created: ", newChannel);
+            res.status(201);
+        })
+        .catch((error) => {
+            console.error("Error creating new channel: ", error);
+            res.status(500);
+        });
+});
+
+function isAuthenticated(req, res, next) {
+    if (req.session && req.session.username) {
+      return next();
+    }
+    res.redirect("/login");
+}
+
+
 module.exports = router;
