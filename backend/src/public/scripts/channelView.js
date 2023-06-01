62086fdd1b15f95ee50f619d653f2c8524eaad85
bynned <benny.cascarino@tuni.fi>
2023-06-01 14:19:25 +0300

    Made it into a single function

--- backend/src/public/scripts/channelView.js
+++ backend/src/public/scripts/channelView.js
@@ -1,26 +1,25 @@
 window.addEventListener("load", function () {
-    var div = document.getElementById("Appid");
-    div.classList.add("appear");
-  });
-
-document.addEventListener('DOMContentLoaded', function() {
-    var createChannelButton = document.getElementById('createChannelButton');
-    var createChannelFormContainer = document.getElementById('createChannelFormContainer');
-    var enterChannelKeyContainer = document.getElementById('enterChannelKeyContainer');
-
-    createChannelButton.addEventListener('click', function() {
-        createChannelFormContainer.style.display = 'block';
-        enterChannelKeyContainer.style.display = 'none';
-    });
+  var div = document.getElementById("Appid");
+  div.classList.add("appear");
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var enterChannelKeyButton = document.getElementById('enterChannelKeyButton');
-    var enterChannelKeyContainer = document.getElementById('enterChannelKeyContainer');
-    var createChannelFormContainer = document.getElementById('createChannelFormContainer');
+document.addEventListener("DOMContentLoaded", function () {
+  var createChannelButton = document.getElementById("createChannelButton");
+  var createChannelFormContainer = document.getElementById(
+    "createChannelFormContainer"
+  );
+  var enterChannelKeyButton = document.getElementById("enterChannelKeyButton");
+  var enterChannelKeyContainer = document.getElementById(
+    "enterChannelKeyContainer"
+  );
 
-    enterChannelKeyButton.addEventListener('click', function() {
-        enterChannelKeyContainer.style.display = 'block';
-        createChannelFormContainer.style.display = 'none';
-    });
-})
+  createChannelButton.addEventListener("click", function () {
+    createChannelFormContainer.style.display = "block";
+    enterChannelKeyContainer.style.display = "none";
+  });
+
+  enterChannelKeyButton.addEventListener("click", function () {
+    enterChannelKeyContainer.style.display = "block";
+    createChannelFormContainer.style.display = "none";
+  });
+});
