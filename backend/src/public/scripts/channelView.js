f96e257b3783fc67c3f23e25db2fc594b87004bf
bynned <benny.cascarino@tuni.fi>
2023-06-01 14:16:25 +0300

    Added enter channel key

--- backend/src/public/scripts/channelView.js
+++ backend/src/public/scripts/channelView.js
@@ -6,8 +6,21 @@ window.addEventListener("load", function () {
 document.addEventListener('DOMContentLoaded', function() {
     var createChannelButton = document.getElementById('createChannelButton');
     var createChannelFormContainer = document.getElementById('createChannelFormContainer');
+    var enterChannelKeyContainer = document.getElementById('enterChannelKeyContainer');
 
     createChannelButton.addEventListener('click', function() {
         createChannelFormContainer.style.display = 'block';
+        enterChannelKeyContainer.style.display = 'none';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var enterChannelKeyButton = document.getElementById('enterChannelKeyButton');
+    var enterChannelKeyContainer = document.getElementById('enterChannelKeyContainer');
+    var createChannelFormContainer = document.getElementById('createChannelFormContainer');
+
+    enterChannelKeyButton.addEventListener('click', function() {
+        enterChannelKeyContainer.style.display = 'block';
+        createChannelFormContainer.style.display = 'none';
+    });
+})
