73f6977a0f513aeeb0e88f5c14e7895f508dbb0a
bynned <benny.cascarino@tuni.fi>
2023-06-25 19:20:31 +0300

    Added Flagged posts to channel moderate page

--- backend/src/routes/moderateRouter.js
+++ backend/src/routes/moderateRouter.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const checkUserChannelMembership = require("../middleware/checkUserChannelMembership");
+const Post = require("../models/posts");
 
-router.get("/channels/:id/moderate", isAuthenticated, checkUserChannelMembership, (req, res) => {
-    res.render("moderateChannel.ejs");
+router.get("/channels/:id/moderate", isAuthenticated, checkUserChannelMembership, async (req, res) => {
+  try {
+      const flaggedPosts = await Post.find({ flagged: { $exists: true, $not: { $size: 0 } } });
+
+      res.render("moderateChannel.ejs", { flaggedPosts });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error retrieving flagged posts." });
+  }
 });
 
 
