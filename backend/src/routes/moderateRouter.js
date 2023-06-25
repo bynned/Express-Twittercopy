c86dd96a9df2f3a9f4f2396b70e5e926b1f83841
bynned <benny.cascarino@tuni.fi>
2023-06-25 19:27:22 +0300

    Pass channel name to ejs

--- backend/src/routes/moderateRouter.js
+++ backend/src/routes/moderateRouter.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const checkUserChannelMembership = require("../middleware/checkUserChannelMembership");
 const Post = require("../models/posts");
+const Channel = require("../models/channel");
 
 router.get("/channels/:id/moderate", isAuthenticated, checkUserChannelMembership, async (req, res) => {
   try {
-      const flaggedPosts = await Post.find({ flagged: { $exists: true, $not: { $size: 0 } } });
+    const channelId = req.params.id;
+    const channel = await Channel.findById(channelId);
+    const flaggedPosts = await Post.find({ flagged: { $exists: true, $not: { $size: 0 } } });
 
-      res.render("moderateChannel.ejs", { flaggedPosts });
+      res.render("moderateChannel.ejs", { channelName: channel.name, flaggedPosts });
   } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Error retrieving flagged posts." });
