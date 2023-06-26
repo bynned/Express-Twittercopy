7c6be7881fe5267d9ab0bc5f7fbc9a6015a80b65
bynned <benny.cascarino@tuni.fi>
2023-06-26 13:19:17 +0300

    Update

--- backend/src/routes/moderateRouter.js
+++ backend/src/routes/moderateRouter.js
@@ -8,14 +8,22 @@ router.get("/channels/:id/moderate", isAuthenticated, checkUserChannelMembership
   try {
     const channelId = req.params.id;
     const channel = await Channel.findById(channelId);
-    const flaggedPosts = await Post.find({ flagged: { $exists: true, $not: { $size: 0 } } });
+    const flaggedPosts = await Post.find({ channel: channelId, flagged: { $exists: true, $not: { $size: 0 } } });
+    const flaggedComments = await Post.aggregate([
+      { $match: { channel: channelId } },
+      { $unwind: "$comments" },
+      { $match: { "comments.comflagged": { $exists: true, $ne: [] } } },
+      { $project: { "comments.content": 1, "comments.comflagged": 1, post: "$_id" } },
+    ]);
 
-      res.render("moderateChannel.ejs", { channelName: channel.name, flaggedPosts });
+    res.render("moderateChannel.ejs", { channelName: channel.name, flaggedPosts, flaggedComments });
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error retrieving flagged posts." });
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving flagged posts and comments." });
   }
 });
+
+
 
 
 function isAuthenticated(req, res, next) {
